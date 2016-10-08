1abab2c01e4785787a79e582826de24720979113
Lewis Peckover <lew@lew.io>
2016-10-08 16:20:52 +0700

    add a run puppet script

---
+++ run_puppet.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+function error_exit {
+    echo
+    echo "$@"
+    exit 1
+}
+shopt -s expand_aliases
+alias die='error_exit "Error at ${0}:`echo $(( $LINENO - 1 ))`:"'
+pushd /etc/puppetlabs || die "Can't get to puppet directory /etc/puppetlabs"
+if [ "$1" == "-u" ]; then
+	git pull || die "Error updating git repo"
+	git submodule update --init --recursive
+	shift
+fi
+puppet apply . ${*,1}
